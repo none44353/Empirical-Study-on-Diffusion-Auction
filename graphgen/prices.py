9c43027b53ad25939ddfb066f94a1035e33d74e2
EtaoinWu <etaoin.wu@gmail.com>
2022-05-08 16:54:57 +0800

    fix: prices

--- graphgen/prices.py
+++ graphgen/prices.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import numpy as np
 import random
-from genBase import GraphGen
+from .genBase import GraphGen
 
 class Price_s(GraphGen):
     def __init__(self, m: int, c = 1, gamma = 1):
@@ -12,7 +12,8 @@ class Price_s(GraphGen):
         #  \frac{(k + c) ^ gamma * p_k}{\sum_k {(k + c) ^ gamma * p_k}}
         # where p_k denotes the fraction of nodes with in-degree k
 
-    def getRandomSubset(sampleList, weightList, sze):
+    @staticmethod
+    def getRandomSubset(sampleList, weight, sze):
         targets = set()
         while len(targets) < sze:
             x = random.choices(sampleList, weights = weight)
@@ -21,25 +22,25 @@ class Price_s(GraphGen):
 
     def __call__(self, n: int) -> nx.DiGraph:
         G = nx.DiGraph()
-        if m < 1 or m >= n: 
+        if self.m < 1 or self.m >= n: 
             G.add_nodes_from(range(0, n))
             return G
 
-        G.add_nodes_from(range(0, m))
-        sampleList = list(range(0, m))
-        weight = [pow(c, gamma)] * m
+        G.add_nodes_from(range(0, self.m))
+        sampleList = list(range(0, self.m))
+        weight = [pow(self.c, self.gamma)] * self.m
 
-        for i in range(m, n):
+        for i in range(self.m, n):
             G.add_node(i)
-            targets = getRandomSubset(sampleList, weight, m)
+            targets = Price_s.getRandomSubset(sampleList, weight, self.m)
             for j in targets:
                 degree = G.in_degree(j)
-                wDelta = pow(degree + 1 + c, gamma) - pow(degree + c, gamma)
+                wDelta = pow(degree + 1 + self.c, self.gamma) - pow(degree + self.c, self.gamma)
                 if wDelta > 0:
                     sampleList.append(j)
                     weight.append(wDelta)
                 G.add_edge(i, j)
             sampleList.append(i)
-            weight.append(pow(c, gamma))
+            weight.append(pow(self.c, self.gamma))
 
         return G
